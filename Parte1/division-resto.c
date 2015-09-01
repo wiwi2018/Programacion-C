a6e68db327571521b296ea214f0cd8cc3d425717
C-Lara <claraa@uni.pe>
2015-09-01 13:15:05 -0500

    Como funciona el operador division y resto

---
+++ Parte1/division-resto.c
@@ -0,0 +1,9 @@
+/* Programa acerca del operador division y resto*/
+
+#include <stdio.h>
+
+int main(){
+		printf("17./4=%g\n", 17./4);
+		printf("17/4=%i\n", 17/4);
+		printf("17%%4=%i\n", 17%4);
+}
