e41e302a6167ae3b502dd4d0b6a011eb6d00b277
C-Lara <claraa@uni.pe>
2015-09-01 13:32:56 -0500

    Uso del iterador for en un programa en C

---
+++ Parte1/for_loop.c
@@ -0,0 +1,12 @@
+/*Ejemplo del uso de for en un programa en C
+ *
+ * Compilar de la siguiente manera: gcc -std=c99 for-loop.c
+ */
+#include<stdio.h>
+
+int main(){
+		for (int i=0; i< 10; i++){
+				printf("Mili ->%i\n", i);
+		}
+}
+
