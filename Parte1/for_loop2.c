0841c12614cf0eb84de78e352779ecd193223277
C-Lara <claraa@uni.pe>
2015-09-01 13:40:40 -0500

    Uso del iterador for  en un programa en C(mejorar archivo)

---
+++ Parte1/for_loop2.c
@@ -0,0 +1,10 @@
+/* Otra forma de aplicar el iterador
+ * for en C
+ * Para compilar, hacer lo siguiente: gcc -std=c99 for_loop2.c
+ */
+
+#include<stdio.h>
+
+int main(){
+		for (int i =0; i < 10; i++) printf("Mili #%i\n", i);
+}
