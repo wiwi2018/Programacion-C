4d17df85823c1ef852f15f16af5df039a8240ad1
C-Lara <claraa@uni.pe>
2015-10-15 22:15:08 -0500

    Niveles de indireccion

---
+++ Parte2/Ejemplo8.c
@@ -0,0 +1,23 @@
+/* El siguiente programa es un ejemplo en que se manipulan punteros
+ con varios niveles
+ de indireccion
+ */
+
+#include <stdio.h>
+int main()
+{
+		int i;
+		int *ptrToi;
+		int **ptrToPtrToi;
+
+		ptrToPtrToi = &ptrToi;
+		ptrToi  = &i;
+
+		i = 10;        // Asignacion directa
+		*ptrToi = 20;  // Asignacion indirecta
+		**ptrToPtrToi = 30; // Asignacion con doble indireccion
+
+		return 0;
+}
+
+
