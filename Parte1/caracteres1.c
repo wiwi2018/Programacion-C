48956888edb002f0bd9ebc136cbf5900ce484c49
C-Lara <claraa@uni.pe>
2015-10-25 01:35:31 -0500

    Conversion de valores numericos a caracteres

---
+++ Parte1/caracteres1.c
@@ -0,0 +1,13 @@
+/*  Conversion de valores numericos a caracteres */
+
+#include <stdio.h>
+main()
+{
+  char c1;
+  char c2;
+  c1 = 65;
+  c2 = 97;
+  printf("El caracter que tiene el valor numero de 65 es: %c.\n", c1);
+  printf("El caracter que tiene el valor numero de 97 es: %c.\n", c2);
+    return 0;
+ }
