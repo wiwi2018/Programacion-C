1d6c352a44ac3299cb34907a0b0d95b8e063cf81
C-Lara <claraa@uni.pe>
2015-10-25 01:09:21 -0500

    Imprimiendo caracteres con printf

---
+++ Parte1/caracteres.c
@@ -0,0 +1,13 @@
+/*Imprimiendo caracteres */
+
+#include <stdio.h>
+main()
+{
+  char c1;
+  char c2;
+  c1 = 'A';
+  c2 = 'a';
+  printf("Convertir el valor de c1 a caracter: %c.\n", c1);
+  printf("Convertir el valor de c2 a caracter: %c.\n", c2);
+    return 0;
+}
