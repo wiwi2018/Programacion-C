4236b1717e9a70b93d6d9cbb4de7500a675681e3
C-Lara <claraa@uni.pe>
2015-10-25 01:37:20 -0500

    Mostrando el valor numerico de caracteres

---
+++ Parte1/caracteres2.c
@@ -0,0 +1,14 @@
+/* Mostrando el valor numerico de los caracteres*/
+#include <stdio.h>
+
+main()
+{
+  char c1;
+  char c2;
+
+   c1 = 'A';
+   c2 = 'a';
+   printf("El valor numerico de A es: %d.\n", c1);
+   printf("El valor numerico de a es: %d.\n", c2);
+   return 0;
+ }
