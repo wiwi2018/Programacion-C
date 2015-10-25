fcfa0382dc0a5f83de1e5600fb0c2a11256a4aa2
C-Lara <claraa@uni.pe>
2015-10-25 11:09:21 -0500

    Ejemplo del  manejo de formato para un entero

---
+++ Parte1/formato1.c
@@ -0,0 +1,19 @@
+/* Especificando  el formato de un entero*/
+
+ #include <stdio.h>
+main()
+{
+  int num1, num2;
+  num1 = 12;
+  num2 = 12345;
+  
+  printf("%d\n", num1);
+  printf("%d\n", num2);
+  printf("%5d\n", num1);
+  printf("%05d\n", num1);
+  printf("%2d\n", num2);
+  
+  return 0;
+
+  
+}
