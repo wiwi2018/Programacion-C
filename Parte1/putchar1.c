b5472ad13ee6914a7dc1b9753e007cbc7dfa9cb2
C-Lara <claraa@uni.pe>
2015-10-25 15:15:51 -0500

    Uso de putchar() y caracteres

---
+++ Parte1/putchar1.c
@@ -0,0 +1,15 @@
+/*Mostrando en caracteres con  putchar() */
+
+#include <stdio.h>
+main()
+{
+  putchar(65);
+  putchar(10);
+   putchar(66);
+    putchar(10);
+    putchar(67);
+  putchar(10);
+
+return 0;
+
+}
